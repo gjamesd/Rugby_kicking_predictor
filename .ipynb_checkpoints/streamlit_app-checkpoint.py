7a03ccafde763703cae19a8b2246629075e2ea11
James Griffin <jamesgriffd@gmail.com>
2023-09-15 15:39:03 -0600

    mods
    mods

--- .ipynb_checkpoints/streamlit_app-checkpoint.py
+++ .ipynb_checkpoints/streamlit_app-checkpoint.py
@@ -30,7 +30,7 @@ def fig1(data):
     fig = px.scatter(
     x=data['x_meters'],
     y=data['y_meters'],
-   color = data['fill'],
+   color = data['result'],
    opacity= data['opacity'], 
    title="Pitch Diagram With All Scrapped Data",
                     labels = {"color":'Make or Miss', 
